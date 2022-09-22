5a739e2089184377edd3f79aafacaa6e3dcc048a
xSky0815x <84250128+xSky0815x@users.noreply.github.com>
2022-09-22 08:09:56 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,23 +3,38 @@ var formidable = require('formidable');
 var fs = require("fs");
 var jsonfile = require("jsonfile");
 
-
 var current_data = {}
 
-const PORT = 8080;
 const DIR = __dirname;
 
-http.createServer(function (req, res) {
+var PORT = 8080;
+if (process.argv.length >= 3) {
+  PORT = process.argv[2];
+}
+
+var server = http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
       if(files.filetoupload  == undefined || files.filetoupload.size == 0) {
-        res.write("error, no data providet")
-        return res.end();
+        fs.readFile("./res/error.html", (_err, data)=>{
+          res.write(data);
+          return res.end();
+        });
       }
+
       var fileCode = getRandomCode(100000, 999999);
       var filepath = (files.filetoupload.filepath);
       var newfilepath = DIR + "/storage/" + String(fileCode) + "/" + files.filetoupload.originalFilename;
+
+      current_data[String(fileCode)] = {
+        "filename": files.filetoupload.originalFilename,
+        "filesize": files.filetoupload.size,
+        "originaltime": files.filetoupload.size,
+        "timeremain": fields.deleteduration,
+        "remoteIP": req.socket.remoteAddress
+
+      }
 
       fs.mkdirSync(DIR + "/storage/"+fileCode, (err) => {
         if (err) {
@@ -29,33 +44,61 @@ http.createServer(function (req, res) {
 
       fs.rename(filepath, newfilepath, function (err) {
         if (err) throw err;
-          fs.readFile("./res/fileuploaded.html", (err, data)=>{
+          fs.readFile("./res/fileuploaded.html", (_err, data)=>{
             data = String(data).replace("######", fileCode)
             res.write(data);
             return res.end();
-          })
+          });
       });
     });
 
-  } else if (req.url.startsWith("/download/")){
+  } else if (req.url.startsWith("/getfile/")){
     code = req.url.split("/")[2];
-    if (code.length !=6) {
-      res.write("error 404 - file not found");
-      return res.end();
-    } else {
-      fs.readdir(DIR+"/storage/", (err, files)=>{
-        if ((String(code) in files) == false) {
-          res.write("error 404 - file not found");
+    
+    if(current_data[code]) {
+
+      filepath = DIR + "/storage/" + code + "/" + current_data[code]["filename"];
+      fs.readFile(filepath, (err, data)=> {
+        if (err) {
+          fs.readFile("./res/error.html", (_err, data)=>{
+            res.write(data);
+            return res.end();
+          });
+        } else {
+          res.setHeader(
+            "Content-Disposition", 'attachment; filename="'+current_data[code]["filename"]+'"',
+            );
+          res.write(data);
           return res.end();
         }
       })
-  
-      res.write(code);
-      return res.end();
-  
+
+    } else  {
+      fs.readFile("./res/error.html", (_err, data)=>{
+        res.write(data);
+        return res.end();
+      });
     }
 
-    
+  } else if (req.url.startsWith("/download/")) {
+    code = req.url.split("/")[2];
+    if(current_data[code]) {
+      fs.readFile("./res/fileinformation.html", (_err, data)=>{
+        data = String(data)
+        .replace("######", code)
+        .replace("??????b", current_data[code]["filesize"]+"b")
+        .replace("file.name", current_data[code]["filename"])
+
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile("./res/error.html", (_err, data)=>{
+        res.write(data);
+        return res.end();
+      });
+    }
+
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
     fs.readFile("./res/index.html", (err, data)=>{
@@ -64,14 +107,17 @@ http.createServer(function (req, res) {
     });
     
   }
-}).listen(PORT); 
+});
+
+server.listen(PORT);
+
 console.info("running on port "+PORT);
 
+setInterval(()=>{processFiles()}, 60000)
 
 
 function getRandomCode() {
-  data = jsonfile.readFileSync("./storage.json");
-    var existing_numbers = Object.keys(data);
+    var existing_numbers = Object.keys(current_data);
     do {
       var randomnumber = random(100000, 999999);
       if (false ==(String(randomnumber) in existing_numbers)) {
@@ -86,3 +132,23 @@ function random(min, max) {
     Math.random() * (max - min + 1) + min
   )
 }
+
+function processFiles(){
+  var todelete = [];
+
+  Object.entries(current_data).forEach(([key, value]) => {
+    if (value["timeremain"] >1) {
+      value["timeremain"] -=1;
+    } else if (value["timeremain"] == 1) {
+      todelete.push(key);
+    }
+  });
+
+  for (let i = 0; i < todelete.length; i++) {
+    fs.rm("./storage/"+todelete[i], { recursive: true });
+    delete current_data[i];
+  }
+
+  jsonfile.writeFile("./current_data.json", current_data);
+}
+
